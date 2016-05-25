4bc7fb4aeeeba60cd0eb91b3e4650fbb179fb881
denisvreshtazi <denisvreshtazi@yahoo.com>
2016-05-25 17:40:42 +0100

    Add files via upload

---
+++ heuristic.py
@@ -0,0 +1,75 @@
+import utils
+import random
+import games
+
+def h0(state):
+    return random.randint(-100,100)
+
+def h1(state):
+    if (state.utility == 1):
+        return state.utility*1000
+    elif (state.utility == -1 ):
+        return state.utility*1000
+    else:
+        return random.randint(-100,100)
+
+def h2(state):
+    player = games.to_move()
+    enemy = plenemy(player)
+    h = 0
+    if (state.utility == 1):
+        return state.utility * 1000
+    elif(state.utility == -1):
+        return state.utility * 1000
+
+    for (x,y) in legal_moves(state):
+        if x==4:
+            h-=10
+        elif x == 3 or x == 5:
+            h-=5
+
+        h += k_in_row(state.board, (x,y), player, (0,1) )
+        h += k_in_row(state.board, (x,y), player, (1,0) )
+        h += k_in_row(state.board, (x,y), player, (1,1) )
+        h += k_in_row(state.board, (x,y), player, (1,-1) )
+
+        h -= k_in_row(state.board, (x,y), enemy, (0,1) )
+        h -= k_in_row(state.board, (x,y), enemy, (0,1) )
+        h -= k_in_row(state.board, (x,y), enemy, (0,1) )
+        h -= k_in_row(state.board, (x,y), enemy, (0,1) )
+
+    return h
+
+
+def legal_moves(state):
+    return [(x, y) for (x, y) in state.moves
+            if y == 1 or (x, y - 1) in state.board]
+
+def plenemy(player):
+    if player == 'X':
+        return 'O'
+    else:
+        return 'X'
+
+
+def k_in_row(board, (x,y), player, (delta_x, delta_y) ):
+    enemy = plenemy(player)
+    x, y = (x,y)
+    n = 0
+    while (board.get((x, y)) != enemy )  and x < 8 and y < 7  :
+        if(board.get((x, y)) == player):
+            n += 4
+        else:
+            n+=2
+        x, y = x + delta_x, y + delta_y
+    x, y = (x,y)
+    while (board.get((x, y)) != enemy )  and x > 0 and y > 0 :
+        if (board.get((x, y)) == player):
+            n += 4
+        else:
+            n += 2
+        x, y = x - delta_x, y - delta_y
+    return n
+
+
+
---
+++ run.py
@@ -0,0 +1,64 @@
+import games
+import heuristic
+
+game = games.ConnectFour()
+state = game.initial
+
+dificulty = 0
+while (dificulty == 0):
+    dif = raw_input("Select dificulty: 1 (Facil), 2 (Medium), 3 (Dificil): ")
+    dificulty = int(str(dif).strip())
+    if (dificulty == 1):
+        h = heuristic.h0
+    elif (dificulty == 2):
+        h = heuristic.h1
+    elif (dificulty == 3):
+        h = heuristic.h2
+    else :
+        print "Dificulty not valid ,please try again ..."
+        dificulty = 0
+
+
+
+
+primo = raw_input("Who start first: 1 (Machine  or 2 (Person) ) ")
+first = int(str(primo).strip())
+
+if(first == 1):
+    player = 'X'
+elif(first == 2):
+    player = 'O'
+else:
+    print("Input not legal! Machine start first! ")
+    player = 'X'
+
+
+while True:
+    print "Jugador a mover:", game.to_move(state)
+    game.display(state)
+
+    if player == 'O':
+        col_str = raw_input("Movimiento: ")
+        coor = int(str(col_str).strip())
+        x = coor
+        y = -1
+        legal_moves = game.legal_moves(state)
+        for lm in legal_moves:
+            if lm[0] == x:
+                y = lm[1]
+
+        state = game.make_move((x, y), state)
+        player = 'X'
+    else:
+        print "Thinking..."
+        #move = games.minimax_decision(state, game)
+        #move = games.alphabeta_full_search(state, game)
+        move = games.alphabeta_search(state, game, d = 4, eval_fn = h)
+
+        state = game.make_move(move, state)
+        player = 'O'
+    print "-------------------"
+    if game.terminal_test(state):
+        game.display(state)
+        print "Final de la partida"
+        break
